a2df9b20e0487406c56e7738b201a6713c6ee3a1
2w <hsp003636@gmail.com>
2024-01-26 21:21:38 -0500

    add SIGNAL handler

--- deauth_attack.c
+++ deauth_attack.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <signal.h>  // 시그널 관련 헤더 추가
 
 #include "deauth.h"
 
@@ -13,10 +14,11 @@ void usage();
 void AP_broadcast_frame(struct Packet *packet);
 void AP_unicast_frame(struct Packet *packet, char *station_mac);
 void Station_unicast_frame(struct Packet *packet, char *ap_mac, char *station_mac);
-void auth_init(struct Auth_Packet *packet, char *ap_mac, char *station_mac);
 
+void auth_init(struct Auth_Packet *packet, char *ap_mac, char *station_mac);
 void initPacket(struct Packet *packet, char *ap_mac);
 void macStringToUint8(char *mac_string, uint8_t *ap_mac);
+void suspend(int signal, pcap_t *handle);
 
 int main(int argc, char *argv[]){
     if (argc < 3){
@@ -56,12 +58,13 @@ int main(int argc, char *argv[]){
     }
 
     time_t start_time = time(NULL);
-    while ((time(NULL) - start_time) < 2) {
+    while ((time(NULL) - start_time) < 10) {
+        suspend(SIGINT, handle);
         if (pcap_sendpacket(handle, (unsigned char *)&packet, sizeof(packet)) != 0){
             printf("send fail\n");
             exit(-1);
         }
-        usleep(10000);
+        sleep(1); // or usleep(10000); ?
     }
 
     pcap_close(handle);
@@ -73,6 +76,7 @@ int main(int argc, char *argv[]){
 // 폰 MAC : 6E:16:EE:4E:47:FA
 // 태블릿 MAC : 2A:AE:C4:FA:C9:1A
 void usage(){
+    printf("Syntax is incorrect.\n");
     printf("syntax : deauth-attack <interface> <ap mac> [<station mac> [-auth]]\n");
     printf("sample : deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
 }
@@ -134,4 +138,11 @@ void macStringToUint8(char *mac_string, uint8_t *ap_mac){
            &ap_mac[0], &ap_mac[1], &ap_mac[2],
            &ap_mac[3], &ap_mac[4], &ap_mac[5]);
 }
-  
+
+
+void suspend(int signal, pcap_t *handle){
+    printf("Entered Ctrl+C, exit program\n");
+    printf("pcap close!");
+    pcap_close(handle);
+    exit(0);
+}
